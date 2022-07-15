efe207f4546d497ead46e06b3386b7e8dbf284d7
WJJ1125 <3154189600@qq.com>
2022-07-15 19:44:26 +0800

    版本冲突第一次测试

--- src/main/java/com/cssl/controller/voteListController.java
+++ src/main/java/com/cssl/controller/voteListController.java
@@ -1,5 +1,4 @@
 package com.cssl.controller;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -8,17 +7,11 @@ import com.cssl.pojo.Ticket;
 import com.cssl.pojo.User;
 import com.cssl.service.StatisticsService;
 import com.cssl.service.TicketService;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class voteListController {
